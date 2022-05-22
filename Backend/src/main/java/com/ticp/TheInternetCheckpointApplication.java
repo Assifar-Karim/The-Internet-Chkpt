3dc5527a97b3a9d51e28e7394cc76865a94549ec
Karim Assifar <82780260+Assifar-Karim@users.noreply.github.com>
2022-05-22 11:56:40 +0100

    Initialize backend repo

---
+++ Backend/src/main/java/com/ticp/TheInternetCheckpointApplication.java
@@ -0,0 +1,13 @@
+package com.ticp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheInternetCheckpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheInternetCheckpointApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ticp/TheInternetCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheInternetCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
