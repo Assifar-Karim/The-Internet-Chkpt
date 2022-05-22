77b559548bb0950b2389d4f870dd6ab6219569c1
Karim Assifar <82780260+Assifar-Karim@users.noreply.github.com>
2022-05-22 18:46:28 +0100

    Add MongoDB configuration
    I added the mongodb configuration files to the project

--- Backend/src/main/java/com/ticp/TheInternetCheckpointApplication.java
+++ Backend/src/main/java/com/ticp/TheInternetCheckpointApplication.java
@@ -2,8 +2,10 @@ package com.ticp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class TheInternetCheckpointApplication {
 
 	public static void main(String[] args) {
