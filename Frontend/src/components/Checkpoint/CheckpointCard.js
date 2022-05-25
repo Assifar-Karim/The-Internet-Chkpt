a1b331b69cd96175ea2f27e8ff3d6cc31d245f9b
iliass-dahman <iliassdahman@gmail.com>
2022-05-25 04:55:46 +0100

    Create frontend for Checkpoints with fake data - added Frontend/components/Checkpoint - some images were moved to assets - modified MainPage.js, logo has now relative position - installed json-server for fake data

---
+++ Frontend/src/components/Checkpoint/CheckpointCard.js
@@ -0,0 +1,21 @@
+import "./CheckpointCard.css";
+import clock from "../../assets/icons/clock.png";
+
+const CheckpointCard = () => {
+    return(
+        <div className="Card" >
+            <div className="Card-header">
+                <span>Checkpoint</span>
+                <div className="time">
+                    <img src={clock} alt="time" />
+                    <span>12/05/2022</span>
+                    <span>12:00</span>
+                </div>
+            </div>
+            <hr></hr>
+            <p>Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2Checkpoint 2</p>
+        </div>
+    );
+}
+
+export default CheckpointCard;
--- Frontend/src/pages/Checkpoints/Checkpoints.js
+++ Frontend/src/pages/Checkpoints/Checkpoints.js
@@ -1,3 +1,52 @@
+import React, { useEffect, useState } from "react";
+import background from "../../assets/backgrounds/main-bg.png";
+import memcard from "../../assets/icons/memcard.png";
+import Logo from "../../components/Logo/Logo";
+import Navbar from "../../components/Navbar/Navbar";
+import CheckpointCard from "../../components/Checkpoint/CheckpointCard";
+import { routes } from "../../Routes";
+import clock from "../../assets/icons/clock.png";
+import darkSools from "../../assets/icons/dark-souls-bonfire.gif";
+import "./Checkpoints.css";
+import { useCallback } from "react";
+
+
 export default function Checkpoints() {
-  return <>Checkpoints</>;
+  
+  const [checkpoints, setCheckpoints] = useState([]);
+  // let list=[1,2,3,4,5,6,7]
+  useEffect(()=>{
+    fetch('http://localhost:8000/checkpoints').then(res=>res.json()).then(data=>{
+      setCheckpoints(data);
+      console.log(data)
+    })
+  },[])
+  return (
+    <>
+      <div className="background">
+        <img src={background} alt="background" />
+      </div>
+      
+      
+      <div className="grid">
+        <Logo />
+        <Navbar links={routes} />
+      </div>
+      <div className="Card" >
+            <textarea id="w3review" name="w3review" rows="2" cols="50" placeholder="SAVE YOUR PROGRESS">
+              </textarea>
+              
+            <hr></hr>
+            <input type="submit" value="SAVE" />
+        </div> 
+      <div className="Cards">
+          {checkpoints.map(item => <CheckpointCard key={item.id}/>)}
+        </div>
+
+        <div className="footer">
+          <img src={darkSools} alt="clock" />
+          <span>THE INTERNET CHECKPOINT | TICP TEAM 2022</span>
+        </div>
+    </>
+  );
 }
