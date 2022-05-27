134a4f48beb9b2e1818817ce7d97c63d65bd2c9b
Sami IMEGHRI <IMEGHRISAMI@gmail.com>
2022-05-27 21:14:02 +0100

    create single checkpoint page

--- Frontend/src/Routes.js
+++ Frontend/src/Routes.js
@@ -1,6 +1,7 @@
 import AboutUs from "./pages/AboutUs/AboutUs";
 import Checkpoints from "./pages/Checkpoints/Checkpoints";
 import MainPage from "./pages/MainPage/MainPage";
+import SharedCheckpoint from "./pages/SharedCheckpoint/SharedCheckpoint";
 
 export const routes = [
   { title: "", segment: "/", component: MainPage, readerToNav: false },
@@ -9,6 +10,12 @@ export const routes = [
     segment: "/checkpoints",
     component: Checkpoints,
     renderToNav: true,
+  },
+  {
+    title: "Shared checkpoint",
+    segment: "/checkpoints/:id",
+    component: SharedCheckpoint,
+    renderToNav: false,
   },
   {
     title: "About Us",
--- Frontend/src/pages/Checkpoints/Checkpoints.js
+++ Frontend/src/pages/Checkpoints/Checkpoints.js
@@ -10,43 +10,55 @@ import darkSools from "../../assets/icons/dark-souls-bonfire.gif";
 import "./Checkpoints.css";
 import { useCallback } from "react";
 
-
 export default function Checkpoints() {
-  
   const [checkpoints, setCheckpoints] = useState([]);
   // let list=[1,2,3,4,5,6,7]
-  useEffect(()=>{
-    fetch('http://localhost:8000/checkpoints').then(res=>res.json()).then(data=>{
-      setCheckpoints(data);
-      console.log(data)
-    })
-  },[])
+  useEffect(() => {
+    fetch("http://localhost:8000/checkpoints")
+      .then((res) => res.json())
+      .then((data) => {
+        setCheckpoints(data);
+      });
+  }, []);
   return (
     <>
       <div className="background">
         <img src={background} alt="background" />
       </div>
-      
-      
+
       <div className="grid">
         <Logo />
         <Navbar links={routes} />
       </div>
-      <div className="Card" >
-            <textarea id="w3review" name="w3review" rows="2" cols="50" placeholder="SAVE YOUR PROGRESS">
-              </textarea>
-              
-            <hr></hr>
-            <input type="submit" value="SAVE" />
-        </div> 
-      <div className="Cards">
-          {checkpoints.map(item => <CheckpointCard key={item.id}/>)}
-        </div>
+      <div className="Card">
+        <textarea
+          id="w3review"
+          name="w3review"
+          rows="2"
+          cols="50"
+          placeholder="SAVE YOUR PROGRESS"
+        ></textarea>
 
-        <div className="footer">
-          <img src={darkSools} alt="clock" />
-          <span>THE INTERNET CHECKPOINT | TICP TEAM 2022</span>
-        </div>
+        <hr></hr>
+        <input type="submit" value="SAVE" />
+      </div>
+      <div className="Cards">
+        {checkpoints.map((checkpoint) => (
+          <CheckpointCard
+            date={checkpoint.formattedCheckpointDate}
+            username={checkpoint.username}
+            content={checkpoint.content}
+            id={checkpoint.id}
+            key={checkpoint.id}
+            isShared={false}
+          />
+        ))}
+      </div>
+
+      <div className="footer">
+        <img src={darkSools} alt="clock" />
+        <span>THE INTERNET CHECKPOINT | TICP TEAM 2022</span>
+      </div>
     </>
   );
 }
