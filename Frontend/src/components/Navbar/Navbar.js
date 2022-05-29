f776dc5134d8ccc6c96ed1485c75fe8be448729d
iliass-dahman <iliassdahman@gmail.com>
2022-05-30 00:30:41 +0100

    Implemented song player added songs to assets modified Navbar moved navbar style from mainpage to navbar component added react-icons:4.3.1 added sass:1.52.1

--- Frontend/src/components/Navbar/Navbar.js
+++ Frontend/src/components/Navbar/Navbar.js
@@ -1,10 +1,94 @@
 import { Link } from "react-router-dom";
+import 'react-h5-audio-player/lib/styles.css';
 import "./Navbar.css";
+import alex from "../../assets/sounds/alex-productions.mp3";
+import aubenwelt from "../../assets/sounds/aubenwelt.mp3";
+import bramble_blast from "../../assets/sounds/bramble_blast.mp3";
+import orridors_of_time from "../../assets/sounds/orridors_of_time.mp3";
+import {  useEffect, useState } from "react";
+import React from 'react';
+
+import play_icon from "../../assets/icons/play.png";
+import pause_icon from "../../assets/icons/pause.png";
+import next_icon from "../../assets/icons/next.png";
+import back_icon from "../../assets/icons/back.png";
+
 
 const Navbar = ({ links }) => {
+
+  
+  const musicTracks=[alex,aubenwelt,bramble_blast,orridors_of_time];
+
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [track, setTrack] = useState(new Audio(musicTracks[trackIndex]));
+  const [isPlaying, setStatePlay] = useState(false);
+
+
+  track.autoplay=true;
+
+  const handleClickPrevious = () => {
+    track.pause();
+    setStatePlay(true);
+    setTrackIndex((currentTrack) =>
+      currentTrack === 0 ? musicTracks.length - 1 : currentTrack - 1
+    );
+  };
+
+  const playMusic=(isPlaying)=>{
+    
+    if (isPlaying) {
+      track.loop=true;
+      track.play();
+    } else {
+      track.pause();
+    }
+  };
+  const handleClickNext = () => {
+    console.log("handleClickNext bf",track,isPlaying);    
+    track.pause();
+    setStatePlay(true);
+    setTrackIndex((currentTrack) =>
+      currentTrack < musicTracks.length - 1 ? currentTrack + 1 : 0
+    );
+    
+    
+  };
+
+
+  
+  const handelClickPlay = () => {
+    console.log("handelClickPlay",track,isPlaying);
+    track.loop=true;
+    track.play();
+    setStatePlay(true);
+  };
+
+  const handelClickPause = () => {
+    console.log("handelClickPause",track,isPlaying);
+    track.pause();
+    setStatePlay(false);
+  };
+
+  useEffect(() => {
+    
+      console.log("effect",track,isPlaying);
+      setTrack(new Audio(musicTracks[trackIndex + 1]))
+  }, [trackIndex]);
+
+  useEffect(() => {
+      console.log("effect track",track,isPlaying);
+      track.play();
+  }, [track]);
+
   return (
+    <div className="navbar-container" >
     <div className="navbar">
-      <ul>
+      
+      <div className="Player-container">
+        <img className="prev" src={back_icon} onClick={handleClickPrevious} />
+        {!isPlaying ? <img className="play" src={play_icon} onClick={handelClickPlay} /> : <img className="play" src={pause_icon} onClick={handelClickPause} />}
+        <img className="next" src={next_icon} onClick={handleClickNext} />
+      </div>
         {links
           .filter((link) => link.renderToNav)
           .map((link, idx) => {
@@ -14,8 +98,9 @@ const Navbar = ({ links }) => {
               </li>
             );
           })}
-      </ul>
-      <hr />
+      
+    </div>
+    <hr />
     </div>
   );
 };
