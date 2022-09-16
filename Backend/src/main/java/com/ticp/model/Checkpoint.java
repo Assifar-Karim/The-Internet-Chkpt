cbb05ecb91bce5d183e2fc377ccbe7aecc4c74fa
Karim Assifar <82780260+Assifar-Karim@users.noreply.github.com>
2022-09-16 17:49:09 +0100

    Refactor Checkpoint feature
    I refactored some parts of the checkpoint feature by modifying the
    way we deal with our data transfer objects and I also added a new
    index on our checkpoint collection to optimize our DB queries.

--- Backend/src/main/java/com/ticp/model/Checkpoint.java
+++ Backend/src/main/java/com/ticp/model/Checkpoint.java
@@ -1,23 +1,25 @@
 package com.ticp.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
-@Document("checkpoint")
-public class Checkpoint {
-
+@Document("checkpoints")
+public class Checkpoint
+{
     @Id
     private String id;
     private String content;
+    @Indexed
     private String userId;
     private Date checkpointDate;
 
     public Checkpoint() {}
 
-    public Checkpoint(String id, String content, String userId, Date checkpointDate) {
-        this.id = id;
+    public Checkpoint(String content, String userId, Date checkpointDate)
+    {
         this.content = content;
         this.userId = userId;
         this.checkpointDate = checkpointDate;
